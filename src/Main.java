e6206e0b5bc4a0b8ce74a8a4d1ccca41f57521f4
Isaac Wu <isaacwu2000@gmail.com>
2025-08-04 12:21:33 -0400

    created cheatsheet for 3x3

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,36 @@ public class Main {
             this.x = x;
             this.y = y;
         }
+        @Override
+        public String toString() {
+            return "(" + Integer.toString((Integer) x) + ", " + Integer.toString((Integer) y) + ")";
+        }
     }
 
     private static class CheatReference {
         public int[] position = new int[n];
-        public Move move;
+        public Move winningMove;
+        public boolean lost = false;
+        // If there is a winning move
         CheatReference(int[] position, Move move) {
             this.position = position;
-            this.move = move;
+            this.winningMove = move;
         }
-        // Create easy way to print this out
+        // if there is a winning move
+        CheatReference(int[] position, boolean lost) {
+            this.position = position;
+            this.lost = true;
+        }
+        @Override
+        public String toString() {
+            if (lost) {
+                return "position: " + Arrays.toString(position) + ", winning move: none";
+            }
+            else {
+                return "position: " + Arrays.toString(position) + ", winning move: " + winningMove.toString();
+            }
+
+        }
     }
 
     // Overcomplicated recursive method of getting a list of all possible board positions
@@ -62,17 +82,48 @@ public class Main {
         ArrayList<Move> validMoves = new ArrayList<Move>();
         for (int col=0; col<position.length; col++) {
             for (int row=0; row<position[col]; row++) {
-                validMoves.add(new Move(col, row));
+                if (col!=0 || row!=0) {
+                    validMoves.add(new Move(col, row));
+                }
             }
         }
         return validMoves;
     }
 
     public static void main(String[] args) {
-        //setPositions(n, n, new ArrayList<Integer>());
-        int[] pos = {2,1,0};
-        for (Move move : getValidMoves(pos)) {
-            System.out.println("x: "+Integer.toString((Integer)move.x)+", y: "+Integer.toString((Integer)move.y));
+        setPositions(n, n, new ArrayList<Integer>()); // The ArrayList 'positions' now contains all the positions
+        positions.remove(0); // Removing [0,0,0..
+        ArrayList<CheatReference> CheatReferences = new ArrayList<CheatReference>();
+        CheatReferences.add(new CheatReference(positions.get(0), true));
+
+        // Iterating over all the positions and seeing if there is a winning move for each position
+        for (int i=1; i<positions.size(); i++) {
+            int[] position = positions.get(i);
+            boolean winningPosition = false;
+
+            ArrayList<Move> validMoves = getValidMoves(position);
+            while (!winningPosition) {
+                for (Move move : validMoves) {
+                    int[] resultingPosition = doMove(position, move);
+                    // Seeing if the move on results in a losing position for the opponent according to the CheatReferences
+                    for (CheatReference ref : CheatReferences) {
+                        if (Arrays.equals(ref.position, resultingPosition) && ref.lost) {
+                            CheatReferences.add(new CheatReference(position, move));
+                            winningPosition = true;
+                            break;
+                        }
+                    }
+                }
+                if (!winningPosition) {
+                    CheatReferences.add(new CheatReference(position, true));
+                    break;
+                }
+            }
+
+        }
+
+        for (CheatReference ref : CheatReferences) {
+            System.out.println(ref);
         }
     }
 }
