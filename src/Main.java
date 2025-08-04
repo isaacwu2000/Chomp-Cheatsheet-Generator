b06993cb5a78ef920b5660929fe1637edcf1a67e
Isaac Wu <isaacwu2000@gmail.com>
2025-08-04 10:33:09 -0400

    converted each position into an array and put it into the arraylist positions

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     private static int n=3;
-    private static ArrayList<Integer[]> positions = new ArrayList<Integer[]>();
+    private static ArrayList<int[]> positions = new ArrayList<int[]>();
 
     // Overcomplicated recursive method of getting a list of all possible board positions
     private static void  loop(int dimension, int n, ArrayList<Integer> cols) {
         for (int i=0; i<=n; i++) {
             cols.add(i);
             if (cols.size()==dimension) {
-                System.out.println(cols);
-                cols.remove(cols.size()-1);
+                // Creating a copy of cols to be an array of integers. This array is then added to the positions
+                int[] colsIntArray = new int[cols.size()];
+                int count = 0;
+                for (Integer col : cols) {
+                    colsIntArray[count] = (int) col;
+                    count++;
+                }
+                positions.add(colsIntArray);
             }
             else {
                 loop(dimension, i, cols);
-                cols.remove(cols.size()-1);
             }
+            cols.remove(cols.size()-1); // Resets the array for the next position
         }
     }
 
     public static void main(String[] args) {
         loop(n, n, new ArrayList<Integer>());
+        for (int[] position : positions) {
+            System.out.println(Arrays.toString(position));
+        }
     }
 }
