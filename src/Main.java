65c0e1843a47885440e2ef236f14d8d18a16e587
Isaac Wu <isaacwu2000@gmail.com>
2025-08-04 09:52:02 -0400

    used recursion to get a list of all possible board positions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
-public class Main {
-    private int[] generateBoardArray() {
+import java.util.ArrayList;
 
+public class Main {
+    private static int n=3;
+    private static ArrayList<Integer[]> positions = new ArrayList<Integer[]>();
+
+    // Overcomplicated recursive method of getting a list of all possible board positions
+    private static void  loop(int dimension, int n, ArrayList<Integer> cols) {
+        for (int i=0; i<=n; i++) {
+            cols.add(i);
+            if (cols.size()==dimension) {
+                System.out.println(cols);
+                cols.remove(cols.size()-1);
+            }
+            else {
+                loop(dimension, i, cols);
+                cols.remove(cols.size()-1);
+            }
+        }
     }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        loop(n, n, new ArrayList<Integer>());
     }
 }
